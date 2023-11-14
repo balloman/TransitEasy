45dbf1be5849a1a6c85d026602d7a5f2a24eac54
Bernard Allotey <benard.allotey4@gmail.com>
2023-11-14 00:06:03 -0600

    Improved accuracy even more by using the raw coordinates provided

--- src/pages/content/components/Map/app.tsx
+++ src/pages/content/components/Map/app.tsx
@@ -1,6 +1,14 @@
 import "@src/styles/index.css";
 import { Show, createSignal } from "solid-js";
 
+/**
+ * Represents a latitude and longitude coordinate pair.
+ */
+interface LatLng {
+  lat: number;
+  lng: number;
+}
+
 function waitForElm(selector) {
   return new Promise((resolve) => {
     if (document.querySelector(selector)) {
@@ -23,11 +31,19 @@ function waitForElm(selector) {
 
 const App = () => {
   const [work, setWork] = createSignal<string | undefined>(undefined);
-  const [building, setBuilding] = createSignal<string>(
-    "One World Trade Center"
-  );
-  waitForElm(".building-title").then((elm) => {
-    setBuilding(elm["innerText"]);
+  const [building, setBuilding] = createSignal<LatLng>({
+    lat: 40.7127,
+    lng: -74.0134,
+  });
+  waitForElm("meta[name='ICBM'").then((elm) => {
+    const coordinates: string | undefined = elm["content"];
+    if (!coordinates) {
+      return;
+    }
+    const latlng = coordinates.trim().split(",");
+    const lat = parseFloat(latlng[0]);
+    const lng = parseFloat(latlng[1]);
+    setBuilding({ lat, lng });
   });
 
   chrome.storage.local.get("address", (result) => {
@@ -43,7 +59,7 @@ const App = () => {
     if (work()) {
       const parsed = work()?.replaceAll(" ", "+");
       // Gets the address by getting the string starting with the first numeric character
-      const parsedBuilding = building().trim().replaceAll(" ", "+");
+      const parsedBuilding = building().lat + "," + building().lng;
       return `https://www.google.com/maps/embed/v1/directions?key=AIzaSyDWXj-Q9-WqQaKqyA48Daz-rYHa8rkDjsk&mode=transit&destination=${parsed}&origin=${parsedBuilding}`;
     }
     return `https://www.google.com/maps/embed/v1/directions?key=AIzaSyDWXj-Q9-WqQaKqyA48Daz-rYHa8rkDjsk&origin=One+World+Tradedestination=Empire+State+Building`;
